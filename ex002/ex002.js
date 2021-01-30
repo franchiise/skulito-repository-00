7cb4ad5dd7337932d62731e64f3e2ce6d66d5cba
skulito <fox04012007@gmail.com>
2021-01-30 17:06:33 -0300

    Enter

--- ex002/ex002.js
+++ ex002/ex002.js
@@ -4,7 +4,7 @@
     usuário, e poderá reproduzi-los um de cada vez com seu valor
     ao quadrado (n ** 2), através da function adicionar().
 
-    Por array estar armazenada dentro de um 'let', permite alte-
+    Por array estar armazenada dentro de um let, permite alte-
     rar seu valor na linha (39), resetando o contador do próprio 
     array, por questões de escopo.
 
@@ -12,6 +12,22 @@
     condicional, que irá garantir a quantidade mínima de números
     (5), e como dito anteriormente, irá rescrever o valor do ar-
     ray para 0, resetando seu resultado. 
+
+    ---------------------------- // ----------------------------
+
+    array, it will receive again all of the numbers entered by the 
+    user, and will be able to reproduce each one of them with 
+    your own values squared over (n ** 2), by the function adici-
+    onar().
+
+    Because array has being stored inside a let, it allows us to
+    change it's value at (39) line, restoring the counter of it-
+    self array, by scope reasons.
+
+    Going forward the end, at the end of the code, we create
+    a conditional structure. Who will get sure a minimun amount
+    of numbers (5), and as it was earlier, it will rewrite the
+    array value for 0, resseting it's result. 
 
 */
 
